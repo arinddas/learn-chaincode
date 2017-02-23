7665c6a42c574ff354cb96a7056e089f1b05065d
arinddas <arinddas@in.ibm.com>
2017-02-23 17:20:26 +0530

    Modified according to requirement

--- NumberPortabilityV1/NumberPortabilityV1.go
+++ NumberPortabilityV1/NumberPortabilityV1.go
@@ -309,6 +309,8 @@ func (t *NumberPortabilityChaincode) EntitlementFromRecipientCSP(stub shim.Chain
 		fmt.Println("Acceptor Service Details Structure",argsNew)
 		
 		
+		
+		
 		UsageDetailsFromDonorandAcceptorCSPObj := UsageDetailsFromDonorandAcceptorCSP{Number: args[0], ServiceProviderOld: args[1], PlanOld: args[2], ServiceValidityOld: args[3], TalktimeBalanceOld: args[4], SMSbalanceOld: args[5], DataBalanceOld: args[6], ServiceProviderNew: argsNew[1], PlanNew: argsNew[2], ServiceValidityNew: argsNew[3], TalktimeBalanceNew: argsNew[4], SMSbalanceNew: argsNew[5], DataBalanceNew: argsNew[6]}
         
 		fmt.Println("Donor+Acceptor Service Details Structure",UsageDetailsFromDonorandAcceptorCSPObj)
@@ -320,23 +322,25 @@ func (t *NumberPortabilityChaincode) EntitlementFromRecipientCSP(stub shim.Chain
 			}
 			
 			
-		fmt.Println("Query EntitlementFromRecipientCSP Chaincode... end") 
-		return []byte(fmt.Sprintf("%s",UsageDetailsFromDonorandAcceptorCSPObj)), nil
+		fmt.Println("Invoke EntitlementFromRecipientCSP Chaincode... end") 
+		//return []byte(fmt.Sprintf("%s",UsageDetailsFromDonorandAcceptorCSPObj)), nil
+		return nil,nil
 	
 	
 
 }
 
+// args should be Number, serviceProviderOld, serviceProviderNew
 
 func (t *NumberPortabilityChaincode) RegulatorQuery(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
     var key, jsonResp string
     var err error
 
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    if len(args) != 3 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 3 arguments")
     }
 
-    key = args[0]
+    key = args[0]+args[1]+args[2]
     valAsbytes, err := stub.GetState(key)
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
@@ -347,6 +351,32 @@ func (t *NumberPortabilityChaincode) RegulatorQuery(stub shim.ChaincodeStubInter
 	}
 
 	fmt.Println("Query NumberPoratbility Chaincode... end") 
+    return valAsbytes, nil 
+
+}
+
+
+// args should be Number, serviceProviderOld, serviceProviderNew
+
+func (t *NumberPortabilityChaincode) EntitlementFromRecipientCSPQuery(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
+
+    if len(args) != 3 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 3 arguments")
+    }
+
+    key = args[0]+args[1]+args[2]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    } else if len(valAsbytes) == 0{
+	    jsonResp = "{\"Error\":\"Failed to get Query for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+	}
+
+	fmt.Println("Query EntitlementFromRecipientCSPQuery ... end") 
     return valAsbytes, nil 
 
 }
@@ -365,8 +395,10 @@ func (t *NumberPortabilityChaincode) Invoke(stub shim.ChaincodeStubInterface, fu
 		return t.EligibilityConfirm (stub, args)
 	} else if function == "UsageDetailsFromDonorCSP" {
 		return t.UsageDetailsFromDonorCSP(stub, args)
+	}else if function == "EntitlementFromRecipientCSP" {
+		return t.EntitlementFromRecipientCSP(stub, args)
 	} else{
-	    return nil, errors.New("Invalid function name. Expecting 'EligibilityConfirm' or 'CSPServiceDetails' but found '" + function + "'")
+	    return nil, errors.New("Invalid function name. Expecting 'EligibilityConfirm' or 'UsageDetailsFromDonorCSP' or 'EntitlementFromRecipientCSP' but found '" + function + "'")
 	}
 	
 	
@@ -384,7 +416,7 @@ func (t *NumberPortabilityChaincode) Query(stub shim.ChaincodeStubInterface, fun
 	fmt.Println("Query NumberPortability Chaincode... start") 
 
 	
-	if function == "EntitlementFromRecipientCSP" {
+	if function == "EntitlementFromRecipientCSPQuery" {
 		return t.EntitlementFromRecipientCSP(stub, args)
 	} 
 	
@@ -397,11 +429,17 @@ func (t *NumberPortabilityChaincode) Query(stub shim.ChaincodeStubInterface, fun
 	var key, jsonResp string
     var err error
 
-    if len(args) != 1 {
+    if len(args) < 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
     }
+	
+	if len(args) == 2{
+	   key = args[0]+args[1]
+	} else{
+	   key = args[0]
+	}
 
-    key = args[0]
+    
     valAsbytes, err := stub.GetState(key)
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
